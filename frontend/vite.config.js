9af854e6d19a6bf450b10a89fc7440981ca09697
jeovan <jeovanbispoo@gmail.com>
2026-03-19 18:10:15 -0300

    Fix: Final Vercel Config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/', 
   resolve: {
     alias: {
-      // Define que o '@' aponta para a pasta 'src'
       '@': path.resolve(__dirname, './src'),
     },
   },
