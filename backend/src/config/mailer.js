78436024fe443472939a1248d39f955593ad65a5
jeovan <jeovanbispoo@gmail.com>
2026-03-21 13:49:22 -0300

    Fix de rede nodemailer

--- backend/src/config/mailer.js
+++ backend/src/config/mailer.js
@@ -1,10 +1,17 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com', // Definindo o servidor expressamente
+  port: 465,
+  secure: true, // Usa conexão SSL
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
+  },
+  // A MÁGICA ESTÁ AQUI:
+  // Essa configuração extra contorna os bloqueios e problemas de rota/IPv6 do Render
+  tls: {
+    rejectUnauthorized: false
   }
 });
 
